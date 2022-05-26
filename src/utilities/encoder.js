bc7b45fc0044bfa5fb4f12321bc16050d2060b1c
Kenwanjohi <wanjohiken85@gmail.com>
2022-05-27 00:28:28 +0300

    Add punycode module

--- src/utilities/encoder.js
+++ src/utilities/encoder.js
@@ -1,10 +1,11 @@
+const Punycode = require("punycode/");
 /**
  * Returns a Hex string with padding
  * @param {number} i integer
  * @returns {string} Hex string
  */
 function dec2hex(i) {
-  return i.toString(16).padStart(4, '0').toUpperCase();
+  return i.toString(16).padStart(4, "0").toUpperCase();
 }
 
 /**
@@ -16,6 +17,19 @@ function hexify(integerArr) {
   return integerArr.map((integer) => dec2hex(integer)).join("");
 }
 
+/**
+ * Converts Unicode characters to ASCII; For IDNs
+ * @param {string} name
+ * @returns A punycode string
+ */
+function punycode(name) {
+  try {
+    return Punycode.toASCII(name);
+  } catch {
+    return name;
+  }
+}
 module.exports = {
   hexify,
+  punycode,
 };
