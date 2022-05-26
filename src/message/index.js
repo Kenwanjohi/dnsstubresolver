130e0d7f346c14045bbd87a521141f4177889b20
Kenwanjohi <wanjohiken85@gmail.com>
2022-05-26 23:29:58 +0300

    Add Message constructor

--- src/message/index.js
+++ src/message/index.js
@@ -1,6 +1,18 @@
 const Header = require("./header");
+
 class Message {
-  //
+  constructor(name, type, klass) {
+    this.header = new Header();
+    this.question = [];
+    this.addQuestion(name, type, klass);
+  }
+
+  addQuestion(name, type, klass) {
+    /**TODO: Add object mapper for codes*/
+    if (!type) type = 0; // A
+    if (!klass) klass = 1; // IN
+    const question = new Question(name, type, klass);
+  }
 }
 
 module.exports = Message;
