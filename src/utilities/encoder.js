10913e098af90298bad901fd7c9ebcfb0198e908
Kenwanjohi <wanjohiken85@gmail.com>
2022-05-28 21:38:02 +0300

    Cleanups: replaced with node Buffer

--- src/utilities/encoder.js
+++ src/utilities/encoder.js
@@ -1,21 +1,4 @@
 const Punycode = require("punycode/");
-/**
- * Returns a Hex string with padding
- * @param {number} i integer
- * @returns {string} Hex string
- */
-function dec2hex(i) {
-  return i.toString(16).padStart(4, "0").toUpperCase();
-}
-
-/**
- * Returns a hex concatenated string
- * @param {number[]} integerArr
- * @returns {string}
- */
-function hexify(integerArr) {
-  return integerArr.map((integer) => dec2hex(integer)).join("");
-}
 
 /**
  * Converts Unicode characters to ASCII; For IDNs
@@ -30,6 +13,5 @@ function punycode(name) {
   }
 }
 module.exports = {
-  hexify,
   punycode,
 };
