4941f734b71c95bf4537e4e1f5a0c518a051cbbc
Kenwanjohi <wanjohiken85@gmail.com>
2022-05-28 21:40:09 +0300

    Fix default qtype

--- src/message/index.js
+++ src/message/index.js
@@ -1,5 +1,5 @@
 const Header = require("./header");
-
+const Question = require("./question");
 class Message {
   constructor(name, type, klass) {
     this.header = new Header();
@@ -9,9 +9,10 @@ class Message {
 
   addQuestion(name, type, klass) {
     /**TODO: Add object mapper for codes*/
-    if (!type) type = 0; // A
+    if (!type) type = 1; // A host adress
     if (!klass) klass = 1; // IN
     const question = new Question(name, type, klass);
+    this.question.push(question);
   }
 }
 
