015d4a71a39a1073445e4d35bb8b2f4a51a9d96b
Kenwanjohi <wanjohiken85@gmail.com>
2022-05-29 01:40:11 +0300

    Add some comments; might need them

--- src/message/question.js
+++ src/message/question.js
@@ -12,30 +12,39 @@ class Question {
   }
   /**
    * Encodes the Question fields
-   * @returns {string} question fields concatenated hexstring
+   * @returns {object} question fields Buffer
    */
   encode() {
     if (this.qname.length > MAX_NAME_LENGTH)
       throw Error`Name too long; Max Length: ${MAX_NAME_LENGTH}`;
     let labels = this.qname.split(".");
 
+    /**
+     * DNS Standard Name notation
+     * Sequence of label-lengths and label
+     * Example: "example.com"
+     *          [7]example[3]com[0]
+     */
     let lengthsLabels = [];
     labels.forEach((label) => {
       if (label.length > MAX_LABEL_LENGTH)
         throw Error`Label too long; Max Length: ${MAX_LABEL_LENGTH}`;
       lengthsLabels.push([label.length], label);
     });
+    /**End of the name; the zero length root */
     lengthsLabels.push([0]);
 
+    /**Encode label-lengths and label characters per byte */
     const lengthsLabelsBuf = [];
     lengthsLabels.forEach((item) => {
       lengthsLabelsBuf.push(Buffer.from(item));
     });
 
     // Well, Maybe there's a better way to do this!!
+    /**Encode qtype and qclass per two bytes */
     const typeClassBuf = Buffer.allocUnsafe(4);
     typeClassBuf.writeUInt16BE(this.qtype, 0);
-    typeClassBuf.writeUInt16BE(this.qclass, 2); 
+    typeClassBuf.writeUInt16BE(this.qclass, 2);
 
     return Buffer.concat([...lengthsLabelsBuf, typeClassBuf]);
   }
