dda8348615313d10c69cd13ad6c40b5efd3c3d16
Kenwanjohi <wanjohiken85@gmail.com>
2022-05-28 21:38:43 +0300

    Encode using node Buffer

--- src/message/question.js
+++ src/message/question.js
@@ -1,6 +1,6 @@
-const { punycode, dec2hex } = require("../utilities/encoder");
-MAX_LABEL_LENGTH = 63
-MAX_NAME_LENGTH=255
+const { punycode } = require("../utilities/encoder");
+MAX_LABEL_LENGTH = 63;
+MAX_NAME_LENGTH = 255;
 class Question {
   constructor(...args) {
     /**Question name; valiable, the query object/domain */
@@ -15,16 +15,29 @@ class Question {
    * @returns {string} question fields concatenated hexstring
    */
   encode() {
-    if(this.qname.length > MAX_NAME_LENGTH ) throw Error `Name too long; Max Length: ${MAX_NAME_LENGTH}`
-    let labels = this.qname.split(".")
-    let encodedname = ""
-    labels.forEach(label => {
-      if (label.length > MAX_LABEL_LENGTH) throw Error `Label too long; Max Length: ${MAX_LABEL_LENGTH}`
-      encodedname += dec2hex(label.length)
-      encodedname += label
-    })
-    //Question bit
-    return encodedname
+    if (this.qname.length > MAX_NAME_LENGTH)
+      throw Error`Name too long; Max Length: ${MAX_NAME_LENGTH}`;
+    let labels = this.qname.split(".");
+
+    let lengthsLabels = [];
+    labels.forEach((label) => {
+      if (label.length > MAX_LABEL_LENGTH)
+        throw Error`Label too long; Max Length: ${MAX_LABEL_LENGTH}`;
+      lengthsLabels.push([label.length], label);
+    });
+    lengthsLabels.push([0]);
+
+    const lengthsLabelsBuf = [];
+    lengthsLabels.forEach((item) => {
+      lengthsLabelsBuf.push(Buffer.from(item));
+    });
+
+    // Well, Maybe there's a better way to do this!!
+    const typeClassBuf = Buffer.allocUnsafe(4);
+    typeClassBuf.writeUInt16BE(this.qtype, 0);
+    typeClassBuf.writeUInt16BE(this.qclass, 2); 
+
+    return Buffer.concat([...lengthsLabelsBuf, typeClassBuf]);
   }
 }
 
