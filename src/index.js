d55467ddaac501e09fbbd278682e445f6a63af0e
Kenwanjohi <wanjohiken85@gmail.com>
2022-06-03 23:58:04 +0300

    encode tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 const Message = require("./message")
+class Resolver {
+
+}
+let messageInstance = new Message("EXAMPLE.com")
+console.log(messageInstance.header.encode())
+console.log("questionBuf",messageInstance.question[0].encode())
+// console.log(Buffer.concat(messageInstance.header.encode() + messageInstance.question[0].encode()))
+console.log(Buffer.concat([messageInstance.header.encode(), messageInstance.question[0].encode() ]).toString('hex'))
