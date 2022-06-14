8f12eb003cc7a79d477368d3466238086d3b966d
Kenwanjohi <wanjohiken85@gmail.com>
2022-06-14 23:59:26 +0300

    decode question from DNS response

--- src/message/index.js
+++ src/message/index.js
@@ -1,5 +1,6 @@
 const Header = require("./header");
 const Question = require("./question");
+const { getQuestion } = require("../utilities/decoder");
 class Message {
   constructor(name, type, klass) {
     this.header = new Header();
@@ -19,9 +20,15 @@ class Message {
 
   decode() {
     let dnsresponse =
-    "65d181800001000100000000076578616d706c6503636f6d0000010001c00c000100010000518700045db8d822";
-    let resHeader = new Header(dnsresponse)
-    return resHeader
+      "65d181800001000100000000076578616d706c6503636f6d0000010001c00c000100010000518700045db8d822";
+    let resHeader = new Header(dnsresponse);
+    const {
+      offset,
+      question: { qname, qtype, qklass },
+    } = getQuestion(dnsresponse, 12);
+    const question = new Question(qname, qtype, qklass);
+    console.log(question)
+    return resHeader;
   }
 }
 
